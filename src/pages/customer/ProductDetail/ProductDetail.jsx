169a20f668e6f2e974b9f3c268e402374a4cf7ab
smiley_12_2 <joseandresgavilanes2012@gmail.com>
2023-01-20 18:38:09 -0500

    aboutus view

---
+++ src/pages/customer/ProductDetail/ProductDetail.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ProductDetail.scss";
+
+const ProductDetail = () => {
+  return <div>ProductDetail</div>;
+};
+
+export default ProductDetail;
--- src/ui/Footer/Footer.jsx
+++ src/ui/Footer/Footer.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import "./Footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer_details">
+        <div className="footer_img">
+          <h2>Kruger</h2>
+        </div>
+        <div className="footer_text">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam
+          vero natus ut nulla, fugiat pariatur!
+        </div>
+      </div>
+
+      <div className="footer_container">
+        <ul className="footer_container_list">
+          <h3>Main</h3>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Home</li>
+          <li>About Us</li>
+        </ul>
+        <ul className="footer_container_list">
+          <h3>Products</h3>
+          <li>All products</li>
+          <li>Cart</li>
+          <li>All products</li>
+          <li>Cart</li>
+        </ul>
+        <ul className="footer_container_list">
+          <h3>Social</h3>
+          <li>All products</li>
+          <li>Cart</li>
+          <li>All products</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
