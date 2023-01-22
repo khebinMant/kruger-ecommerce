8b980639fe624ab80a3686d4d86df072a95819d2
smiley_12_2 <joseandresgavilanes2012@gmail.com>
2023-01-21 20:09:39 -0500

    cart view

---
+++ src/pages/customer/Cart/CartItem/CartItem.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./CartItem.scss";
+
+const CartItem = () => {
+  return (
+    <div class="cart-items">
+      <div class="cart_image-box">
+        <img
+          src="https://www.vodafone.com.au/images/devices/apple/iphone-14-pro-max/iphone-14-pro-max-deep-purple-feature1-m.jpg"
+          style={{ height: "80px" }}
+        />
+      </div>
+      <div class="cart_about">
+        <h1 class="cart_title">Apple Juice</h1>
+        <h3 class="cart_subtitle">250ml</h3>
+        <div className="cart_about_img">
+          <img src="https://www.vodafone.com.au/images/devices/apple/iphone-14-pro-max/iphone-14-pro-max-deep-purple-feature1-m.jpg" />
+        </div>
+      </div>
+      <div class="cart_counter">
+        <div class="cart_btn">-</div>
+        <div class="cart_count">2</div>
+        <div class="cart_btn">+</div>
+      </div>
+      <div class="cart_prices">
+        <div class="cart_amount">$2.99</div>
+
+        <div className="cart_extras">
+          <div class="cart_save">
+            <i class="fa-regular fa-heart"></i>
+          </div>
+          <div class="cart_remove">
+            <i class="fa-solid fa-trash"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
