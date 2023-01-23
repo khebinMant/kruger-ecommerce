c3fb043f9d7b0a556f0430ff3aae3bf205c6fd57
smiley_12_2 <joseandresgavilanes2012@gmail.com>
2023-01-23 17:21:09 -0500

    navbar details, home extras and products and services views

--- src/pages/customer/MainPage/MainPage.jsx
+++ src/pages/customer/MainPage/MainPage.jsx
@@ -5,15 +5,20 @@ import Products from "./Products/Products";
 import HomeExtras from "./HomeExtras/HomeExtras";
 import WhyUs from "./WhyUs/WhyUs";
 import Services from "./Services/Services";
+import MainInfoHome from "./MainInfo/MainInfo";
+import EncourageUser from "./EncourageUser/EncourageUser";
 
 const MainPage = () => {
   return (
     <div className="mainpage">
       <Banner />
+      <MainInfoHome />
+
       <Products />
       <HomeExtras />
       <Services />
       <WhyUs />
+      <EncourageUser />
     </div>
   );
 };
