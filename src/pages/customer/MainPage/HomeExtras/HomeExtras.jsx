14929abb8e09aeb055349caa6150d0e19b0f79f9
smiley_12_2 <joseandresgavilanes2012@gmail.com>
2023-01-26 22:01:24 -0500

    translating text into spanish

--- src/pages/customer/MainPage/HomeExtras/HomeExtras.jsx
+++ src/pages/customer/MainPage/HomeExtras/HomeExtras.jsx
@@ -5,33 +5,35 @@ const HomeExtras = () => {
   return (
     <section className="features">
       <div className="feature">
-        <p className="feature__subtitle">What we offer</p>
-        <h2 className="feature__title">Just sit back at home</h2>
+        <p className="feature__subtitle">¿Qué ofrecemos?</p>
+        <h2 className="feature__title">Encuentra el dispositivo</h2>
         <h2 className="feature__title">
-          we will <span>take care</span>
+          perfecto <span> para ti</span>
         </h2>
         <p className="feature__text">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-          <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Aperiam, eius.
+          Aquí encontrarás una amplia variedad de los últimos y mejores
+          dispositivos inteligentes. Tenemos algo para
+          <br />
+          todos, ya sea que estes buscando el último iPhone o un dispositivo
+          Android asequible.
         </p>
       </div>
 
       <ul className="feature__list">
         <li className="feature__item">
           <img src="/images/g.svg" alt="feature-img" className="feature__img" />
-          <h5 className="feature__item-title">Lot of products</h5>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+          <h5 className="feature__item-title">Variedad de productos</h5>
+          <p>Mantente a la vanguardia de la tecnología con nuestra selección</p>
         </li>
         <li className="feature__item">
           <img src="/images/l.svg" alt="feature-img" className="feature__img" />
-          <h5 className="feature__item-title">Buy from anywhere</h5>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+          <h5 className="feature__item-title">Compra donde sea</h5>
+          <p>Experimenta la comodidad y satisfacción de comprar online</p>
         </li>
         <li className="feature__item">
           <img src="/images/b.svg" alt="feature-img" className="feature__img" />
-          <h5 className="feature__item-title">Safe payment</h5>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+          <h5 className="feature__item-title">Pago seguro</h5>
+          <p>Compre ahora y únase a millones de clientes satisfechos</p>
         </li>
       </ul>
     </section>
--- src/pages/customer/MainPage/MainInfo/MainInfo.jsx
+++ src/pages/customer/MainPage/MainInfo/MainInfo.jsx
@@ -7,23 +7,24 @@ const MainInfoHome = () => {
       <div className="spacer layer2"></div>
 
       <div className="header__info-content">
-        <h3>Easiest way to buy a product</h3>
+        <h3>La forma más fácil de comprar</h3>
         <h2 className="header__info-title">
-          <span>Products?</span> Just wait them at <span>your door</span>
+          <span>Productos?</span> Esperalos en la puerta de{" "}
+          <span> tu casa</span>
         </h2>
 
         <div className="header__info-btns">
-          <button>Order</button>
-          <button>Contact</button>
+          <button>Comprar</button>
+          <button>Explora</button>
         </div>
         <div className="header__info-trust">
           <div>
             <i class="fa-solid fa-sack-dollar"></i>
-            <p>No shipping charge</p>
+            <p>Envío 100% gratis!</p>
           </div>
           <div>
             <i class="fa-solid fa-check"></i>
-            <p>100% secure checkout</p>
+            <p>100% seguro al comprar</p>
           </div>
         </div>
       </div>
