6fc96b7f961c43f25f0fc0bdee09ceee2609389c
khabLexander <mantillagka@hotmail.com>
2023-01-29 12:33:17 -0500

    Bug de la imagen

--- src/ui/CustomerNavBar/UserCircle/UserCicle.jsx
+++ src/ui/CustomerNavBar/UserCircle/UserCicle.jsx
@@ -3,14 +3,14 @@ import "./UserCircle.scss";
 import defaultUserPhoto from "../../../assets/defaultUserPhoto.jpg";
 import { useSelector } from "react-redux";
 const UserCicle = () => {
-  const currentUser=useSelector((state)=>state.users.currentUser);
+  const currentUser = useSelector((state)=>state.users.currentUser);
 
   return (
     <div className="usercicle">
       <div className="usercicle_img">
         <img
-          src={currentUser?(currentUser.imageUrl.startsWith("http")? currentUser.imageUrl : `data:image/jpeg;base64,${currentUser.imageUrl}`) : defaultUserPhoto}
-          alt=""
+          src={currentUser.imageUrl?(currentUser.imageUrl.startsWith("http")? currentUser.imageUrl : `data:image/jpeg;base64,${currentUser.imageUrl}`) : defaultUserPhoto}
+          alt="currentUser"
         />
       </div>
     </div>
