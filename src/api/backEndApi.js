e9864cd82c344faed9c6a4a59a5d2e6dc141efa0
khabLexander <mantillagka@hotmail.com>
2023-02-07 13:44:48 -0500

    Before create branch to deploy

--- src/api/backEndApi.js
+++ src/api/backEndApi.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 //connectar al customer-microservice
 export const backEndApi = axios.create({
-    baseURL: `http://krugercell-service.sistemaagil.net:8082/api/`,
+    baseURL: `http://localhost:8082/api/`,
 }) 
